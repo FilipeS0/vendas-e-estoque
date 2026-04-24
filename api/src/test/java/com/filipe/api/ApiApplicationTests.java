07cc394a057438db9f5ae6f38069802f5a598bb5
Filipe <filipesoutocosta@gmail.com>
2026-04-24 10:10:33 -0300

    Primeiro commit

---
+++ api/src/test/java/com/filipe/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
