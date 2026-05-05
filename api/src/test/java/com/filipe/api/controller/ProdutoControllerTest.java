8e0ff483f98d7a87bbe32a24c18447055d5d464c
Filipe Souto <filipesoutocosta@gmail.com>
2026-05-05 16:17:29 -0300

    test: add unit tests for Venda, Caixa, and Produto controllers using MockMvc

--- api/src/test/java/com/filipe/api/controller/ProdutoControllerTest.java
+++ api/src/test/java/com/filipe/api/controller/ProdutoControllerTest.java
@@ -10,7 +10,7 @@ import com.filipe.api.domain.produto.OrigemProduto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
