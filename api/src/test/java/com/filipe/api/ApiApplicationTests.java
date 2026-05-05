f5df250989933eee730f356db646c29977062c96
Filipe Souto <filipesoutocosta@gmail.com>
2026-05-05 16:01:18 -0300

    test: add basic context loads test for ApiApplication

--- api/src/test/java/com/filipe/api/ApiApplicationTests.java
+++ api/src/test/java/com/filipe/api/ApiApplicationTests.java
@@ -1,13 +1,9 @@
 package com.filipe.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class ApiApplicationTests extends AbstractIntegrationTest {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
