b9d686617ebc9b2382792624edceb09d576efd75
Filipe Souto <filipesoutocosta@gmail.com>
2026-05-05 15:49:14 -0300

    test: add context load test for Spring Boot application configuration

--- api/src/test/java/com/filipe/api/ApiApplicationTests.java
+++ api/src/test/java/com/filipe/api/ApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApiApplicationTests {
+public class ApiApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
